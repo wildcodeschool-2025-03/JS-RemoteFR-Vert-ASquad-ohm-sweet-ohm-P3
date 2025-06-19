86d8be35e71bbdd8eaf40c6ec121cd4e7e5fa7b9
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-06-19 14:36:46 +0200

    feat: adding map image component in HomePage

---
+++ client/src/components/MapImage/mapImage.tsx
@@ -0,0 +1,15 @@
+import "./mapImage.css";
+
+function MapImage() {
+  return (
+    <>
+      <div className="image_map">
+        <button className="map_button" type="button">
+          Voir la carte
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default MapImage;
