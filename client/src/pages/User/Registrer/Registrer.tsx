5ed89aa14ac295b2a6876785dd683dc4d889999e
LAU971 <laury.lutin@gmail.com>
2025-06-30 18:51:49 -0400

    chore: fix formatting to pass biome CI check

--- client/src/pages/User/Registrer/Registrer.tsx
+++ client/src/pages/User/Registrer/Registrer.tsx
@@ -35,7 +35,7 @@ function Register() {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
-        }
+        },
       );
 
       if (response.ok) {
