1f5af06dedb07f7c9614a1c8662e3b2388c0f4ff
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-07-18 09:40:36 +0200

    feat: adding complete reservation for users

--- client/src/pages/Bookings.tsx
+++ client/src/pages/Bookings.tsx
@@ -1,9 +1,11 @@
+import Payment from "../components/Bookings/Payment";
 import TerminalUserInfos from "../components/Bookings/TerminalUserInfos";
 
 function Bookings() {
   return (
     <>
       <TerminalUserInfos />
+      <Payment />
     </>
   );
 }
