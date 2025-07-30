b568562defbf68d61f0db91da2f6d46cecdb059a
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-07-30 10:40:47 +0200

    fix: improving css in homepage

--- client/src/components/review/Review.tsx
+++ client/src/components/review/Review.tsx
@@ -64,8 +64,9 @@ function Review() {
             disableOnInteraction: false,
           }}
           breakpoints={{
+            320: { slidesPerView: 1, spaceBetween: 10 },
             768: { slidesPerView: 2, spaceBetween: 20 },
-            992: { slidesPerView: 3, spaceBetween: 20 },
+            992: { slidesPerView: 3, spaceBetween: 30 },
           }}
           navigation={true}
         >
